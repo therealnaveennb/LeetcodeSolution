5cd6756286aba21d7ccc1b4e3df6aceafd57d517
Naveen bharathi <95202935+therealnaveennb@users.noreply.github.com>
2024-02-04 11:19:25 +0530

    Add files via upload

---
+++ MergeTwoSortedList.java
@@ -0,0 +1,21 @@
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode head = new ListNode(0);
+        ListNode curr = head;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                curr.next = list1;
+                list1 = list1.next;
+            } else {
+                curr.next = list2;
+                list2 = list2.next;
+            }
+            curr = curr.next;
+        }
+
+        curr.next = list1 != null ? list1 : list2;
+        return head.next;
+
+    }
+}
